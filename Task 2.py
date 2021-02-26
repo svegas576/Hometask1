333a4da849149ab0b1ff81cbb56f872f4149e3e9
svegas576 <77957030+svegas576@users.noreply.github.com>
2021-02-26 15:21:52 +0300

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,4 +1,30 @@
-nums = input("Введите числа: ").split(",")
-new_nums = [nums[i] for i in range(0, len(nums)) if nums[i] > nums[i-1]]
-print(new_nums)
+from abc import ABC, abstractmethod
+class Clothes:
 
+    def __init__(self,name):
+        self.name = name
+    @property
+    @abstractmethod
+    def fabrics(self):
+        pass
+
+class Coat():
+    def __init__(self,name,size):
+        self.name = name
+        self.size = size
+    @property
+    def fabrics(self):
+        return round(self.size / 6.5 + 0.5)
+
+class Suit():
+    def __init__(self,name,height):
+        self.name = name
+        self.height = height
+    @property
+    def fabrics(self):
+        return round((2 * self.height + 0.3)/100)
+
+suit = Suit("костюм", 140)
+coat = Coat("пальто", 46)
+print(f'{suit.fabrics}м ткани понадобится для изготовления {suit.name}а')
+print(f'{coat.fabrics}м ткани понадобится для изготовления {coat.name}')
